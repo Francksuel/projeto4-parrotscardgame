b3edb88f9e0a1c31880d00ec828038d499651b30
Francksuel <francksuelbarbosa@gmail.com>
2022-06-19 23:47:48 -0300

    Jogo finalizado com bonûs

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
 let cartas = [
     `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/bobrossparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
-    `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/bobrossparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
-    `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/explodyparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
     `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/explodyparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
     `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/fiestaparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
-    `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/fiestaparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
-    `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/metalparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
     `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/metalparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
     `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/revertitparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
-    `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/revertitparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
     `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/tripletsparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
-    `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/tripletsparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
-    `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/unicornparrot.gif"> <img class="front show" src="./images/front.png"> </div>`,
     `<div class="carta" onclick="virarcarta(this)" ><img class="back" src="./images/unicornparrot.gif"> <img class="front show" src="./images/front.png"> </div>`
 ];
-let selecionadas =[]
-let quant = Number (prompt("Quantas cartas quer jogar? (Número par de 4 a 14)"))
+let selecionadas =[];
+let lista=[];
+let jogadas=0
+let cartasviradas=0
+let stop=0
+let cronometro=0
+let quant=0
+inicio()
+function inicio(){
+quant = Number (prompt("Quantas cartas quer jogar? (Número par de 4 a 14)"))
 while ((quant < 4) | (quant >14) | (quant%2!==0)){
     quant = prompt("Incorreto, tente novamente! (Número par de 4 a 14)")
 }
 //Distribuição de cartas aleatoriamente
-for (let i = 0; i < quant; i++){
-    selecionadas[i]=cartas[i];
+for (let i = 0; i < quant/2; i++){
+    selecionadas.push(cartas[i]);
+    selecionadas.push(cartas[i]);
 }
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 comparador ()
 selecionadas.sort(comparador);  
-for (let i = 0; i < quant; i++) {
+for (let i = 0; i < quant; i++) {    
     document.querySelector(".jogo").innerHTML += `${selecionadas[i]}`
 }
 //Fim da distribuição
-    let cronometro=0
-    let stop=setInterval(relogio,1000)
+    stop=setInterval(relogio,1000)
 function relogio(){
     cronometro++
     document.querySelector(".relogio").innerHTML=`${cronometro}`
 }
-let lista=[];
-let jogadas=0
-let cartasviradas=0
+}
 function virarcarta (elemento) { 
     if (cartasviradas!==2){     
     let valor= elemento.classList.contains("flip")
@@ -80,5 +79,21 @@ function diferente (){
 }
 
 function fimdejogo (){
-    alert (`Parabéns, você ganhou em ${jogadas} jogadas e em ${document.querySelector(".relogio").innerHTML} segundos! `)
+    alert (`Parabéns, você ganhou em ${jogadas} jogadas e em ${document.querySelector(".relogio").innerHTML} segundos! `);
+    let reinicio=prompt ("Quer jogar novamente? (sim/não)");
+    while (reinicio!=="sim" && reinicio!=="não"){
+        reinicio=prompt ("Responda sim ou não!");
     }
+    if (reinicio=="sim"){
+        quant=0
+        selecionadas =[];
+        lista=[];
+        jogadas=0
+        cartasviradas=0 
+        stop=0
+        cronometro=0
+        document.querySelector(".jogo").innerHTML=""  
+        inicio()    
+    }
+    }
+    
